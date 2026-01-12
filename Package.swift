dfa2d8432dfb9c54f2f13b92ba500bb489e723f7
Luke Howard <lukeh@padl.com>
2026-01-12 21:31:13 +1100

    pin swift-subprocess to 0.2.1 or higher

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-system", from: "1.4.0"),
     .package(url: "https://github.com/apple/swift-log", from: "1.6.2"),
-    .package(url: "https://github.com/swiftlang/swift-subprocess", branch: "main"),
+    .package(url: "https://github.com/swiftlang/swift-subprocess", from: "0.2.1"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
